b9e4b0519bee748f72e25b3368664939259b2bcf
Lucas Saliés Brum <sistematico@gmail.com>
2022-09-24 15:44:09 -0400

    Added TailwindCSS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
+import './index.css'
 
-createApp(App).mount('#app')
+const routes = setupLayouts(generatedRoutes)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})  
+
+createApp(App)
+    .use(router)
+    .mount('#app')
+
+
